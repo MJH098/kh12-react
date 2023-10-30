9d41990dcfda3000c2c33db3f85c0c97b8ddab6e
MJH098 <narutoye@naver.com>
2023-10-30 19:44:07 +0900

    예제 3번

---
+++ demo03/src/components/Exam04.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Exam04 = ()=>{
+    const [num, setNum] = useState(0);
+    const numLeng = (e) => {
+    }
+    return (
+        <>
+        <textarea></textarea>
+        {numLeng}/1000
+        </>
+      );
+    };
+
+
+export default Exam04;
