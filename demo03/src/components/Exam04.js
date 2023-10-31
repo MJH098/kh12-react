2b37a6006b652ad269259c8c44da7cdbbf836de7
MJH098 <narutoye@naver.com>
2023-10-31 12:59:10 +0900

    예제 4

--- demo03/src/components/Exam04.js
+++ demo03/src/components/Exam04.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
 const Exam04 = ()=>{
-    const [num, setNum] = useState(0);
-    const numLeng = (e) => {
-    }
-    return (
-        <>
-        <textarea></textarea>
-        {numLeng}/1000
-        </>
-      );
-    };
+    const [number, setNumber] = useState(0);
 
+    
+    return(
+        <>
+            <h1>(Q)주말에 뭐하세요?</h1>    
+            <textarea className="form-control w-50" rows="5" maxLength={1000} onChange={e=>setNumber(parseInt(e.target.textLength))}></textarea>
+            <div className="text-end w-50">
+                <h4>{number}/1000 bytes</h4>
+            </div>
+        </>
+    );
+
+};
 
 export default Exam04;
