f86eb5785d1f8acbf58d72a0629b489d23d0daab
MJH098 <narutoye@naver.com>
2023-11-06 18:22:21 +0900

    포켓몬스터 목록 서버와 연동하여 출력

--- demo05/src/components/Pocketmon.js
+++ demo05/src/components/Pocketmon.js
@@ -1,20 +1,64 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { Modal } from "bootstrap";
 
 const Pocketmon = (props)=>{
     const [pocketmonList, setPocketmonList] = useState([]);
+    //서버에서 pocketmon list를 불러와서 state에 설정하는 코드
+    const loadPocketmon = ()=>{
+        
+            axios({
+                url:"http://localhost:8080/pocketmon/",
+                method:"get"
+            })
+            .then(response=>{
+                // console.log(response);
+                setPocketmonList(response.data);
+            })
+            .catch(err=>{});
+        
+    };
     useEffect(()=>{
-        //서버에서 pocketmon list를 불러와서 state에 설정하는 코드
-        axios({
-            url:"http://localhost:8080/pocketmon/",
-            method:"get"
-        })
-        .then(response=>{
-            // console.log(response);
-            setPocketmonList(response.data);
-        })
-        .catch(err=>{});
+        loadPocketmon();
     },[]);
+
+//포켓몬스터 삭제
+//- 이제는 state에서 삭제하는 것이 아니라 서버에 통신을 보낸 뒤 목록을 갱신하면 된다
+const deletePocketmon = (pocketmon) => {
+    const choice = window.confirm("정말 삭제하시겠습니까?");
+    if(choice === false) return;
+
+    //axios({옵션}).then(성공시 실행할 함수).catch(실패시 실행할 함수);
+    axios({
+        url:`http://localhost:8080/pocketmon/${pocketmon.no}`,
+        method:"delete"
+    })
+    .then(response=>{
+        loadPocketmon();//목록 갱신
+    })
+    .catch(err=>{});
+};
+//modal 관련된 처리
+const bsModal = useRef();
+const openModal = ()=>{
+    const modal = new Modal(bsModal.current);
+    modal.show();
+};
+const closeModal = ()=>{
+    const modal = Modal.getInstance(bsModal.current);
+    modal.hide();
+};
+
+//등록과 관련된 state
+const [pocketmon, setPocketmon] = useState({name:"", type:""});
+const changePocketmon = (e)=>{
+    setPocketmon({
+        ...pocketmon,
+        [e.target.name] : e.target.value
+    });
+};
 
     return(
         <>
@@ -25,6 +69,17 @@ const Pocketmon = (props)=>{
                 </div>
             </div>
 
+            {/* 추가 버튼 */}
+            <div className="row mt-4">
+                <div className="col text-end">
+                    <button className="btn btn-success" onClick={openModal}>
+                        <AiOutlinePlus/>
+                        추가
+                    </button>
+                </div>
+            </div>
+
+            {/* 출력 위치 */}
             <div className="row mt-4">
                 <div className="col">
                     <table className="table">
@@ -42,11 +97,55 @@ const Pocketmon = (props)=>{
                                     <td>{pocketmon.no}</td>
                                     <td>{pocketmon.name}</td>
                                     <td>{pocketmon.type}</td>
-                                    <td></td>
+                                    <td>
+                                        <button type="button" className="btn btn-light btn-sm"><FaEdit/></button>
+                                        <button type="submit" class="btn btn-outline-danger btn-sm"  
+                                                                onClick={e=>deletePocketmon(pocketmon)}>
+                                            <FaTrash/>
+                                        </button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
+                </div>
+            </div>
+
+            {/* Modal */}
+            <div className="modal fade" ref={bsModal}  
+                            data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">제목</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-label">이름</div>
+                                    <input type="text" name="name" className="form-control"
+                                        value={pocketmon.name} onChange={changePocketmon}/>
+                                </div>
+                            </div>
+
+                            <div className="row mt-4">
+                                <div className="col">
+                                    <label className="form-label">속성</label>
+                                    <input type="text" name="type" className="form-control" 
+                                        value={pocketmon.type} onChange={changePocketmon}/>
+                                </div>
+                            </div>
+                        
+                        
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-success">저장</button>
+                        <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
+                    </div>
+                    </div>
                 </div>
             </div>
         </>
