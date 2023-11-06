c97a24284d5bb1e1a4a3aaff388a58f133ee413d
MJH098 <narutoye@naver.com>
2023-11-06 15:21:20 +0900

    포켓몬스터 목록 띄우기

--- demo05/src/components/Book.js
+++ demo05/src/components/Book.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Book = (props)=>{
+
+    const [bookList, setBookList] = useState([]);
+    useEffect(()=>{
+        //서버에서 pocketmon list를 불러와서 state에 설정하는 코드
+        axios({
+            url:"http://localhost:8080/book/",
+            method:"get"
+        })
+        .then(response=>{
+            // console.log(response);
+            setBookList(response.data);
+        })
+        .catch(err=>{});
+    },[]);
+
     return(
         <>
             <h1>도서 관리화면</h1>
